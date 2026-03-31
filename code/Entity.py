c886111d0b93021d29c74dfb27ee7db890326e53
viviane.desiderio <desideriovivis@gmail.com>
2026-03-31 00:15:44 -0300

    creating class abc and background

--- code/Entity.py
+++ code/Entity.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+from abc import ABC, abstractmethod
 
-class Entity:
-    def __init__(self):
+
+class Entity(ABC):
+    def __init__(self, name: str, position: tuple):
         self.name = None
-        self.surf = None
-        self.rect = None
+        self.surf = pygame.image.load('./asset' + name + '.png')
+        self.rect = self.surf.get_rect(left=position[0], top=position[1])
+        self.speed = 0
 
+    @abstractmethod
     def move(self, ):
         pass
