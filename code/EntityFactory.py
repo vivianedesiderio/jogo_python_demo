3a7c2267781b5f29bf6acc94f12926ddb7ed2f68
viviane.desiderio <desideriovivis@gmail.com>
2026-03-31 00:27:32 -0300

    creating EntityFactory

--- code/EntityFactory.py
+++ code/EntityFactory.py
@@ -1,9 +1,16 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+from code.Background import Background
+
 
 class EntityFactory:
-    def __init__(self):
-        pass
 
-    def get_entity(self, entity_type):
+    def get_entity(entity_name: str, position=(0, 0)):
+        match entity_name:
+            case 'level1Bg':
+                list_bg = []
+                for i in range(7):
+                    list_bg.append(Background(f'level1Bg-{i}', position(0, 0)))
+
+
         pass
